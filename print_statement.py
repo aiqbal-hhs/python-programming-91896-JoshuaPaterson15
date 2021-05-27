625ec3842b40c90d49c1469688f79a411d90ebee
JoshuaPaterson15 <65004100+JoshuaPaterson15@users.noreply.github.com>
2021-05-27 13:11:04 +1200

    Basic Print Statement

---
+++ print_statement.py
@@ -0,0 +1,13 @@
+print("Hello and Welcome to this Digital 200 print statement.")
+print("Here is string one." + "Here is string two.")
+print("Here is string one times 5." * 5)
+name = input("What is your name? ")
+print("Welcome to Digital 200 {}.".format(name))
+print("Line one \nLine two \nLine three")
+print("""This is line one
+        this is line two
+        this is line three""")
+
+
+
+
