e443f2b167076a7359344ab7c8bf0d8390d9f474
JoshuaPaterson15 <65004100+JoshuaPaterson15@users.noreply.github.com>
2021-05-28 10:08:36 +1200

    combining_things_in_a_print_statement

---
+++ combining_things_in_a_print_statement.py
@@ -0,0 +1,22 @@
+print("Hello, there. You will be asked a series of questions throughout this survey.")
+name = input("What is your name? ")
+form_class = input("What form class are you in? ")
+transport_cost = input("How much do you pay for Transport on a daily basis? ")
+
+
+
+print("Hello " + name)
+print("Thank you {} for joining {}. Its a fantastic form class.".format(name,form_class))
+print("It appears you pay about the following amount per day $", transport_cost)
+print(" 10 * 10 = {}".format(10 * 10))
+print(" 10 + 10 =", 10 + 10)
+
+
+
+
+
+
+
+
+
+
