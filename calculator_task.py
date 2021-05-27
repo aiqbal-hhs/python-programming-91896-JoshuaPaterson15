78ec9cafa83572dfad8dd7119834529fec13c439
JoshuaPaterson15 <65004100+JoshuaPaterson15@users.noreply.github.com>
2021-05-27 13:10:22 +1200

    Math Calculator Operations

---
+++ calculator_task.py
@@ -0,0 +1,51 @@
+def addition():
+    question1 = float(input("Please type in your first number you would like to use in addition?"))
+    question2 =float(input("Please type in your second number you would like to use in addition?"))
+    print(question1 + question2)
+
+addition()
+
+def subtraction():
+    question1 = float(input("Please type in your first number you would like to use in subtracting?"))
+    question2 =float(input("Please type in your second number you would like to use in subtracting?"))
+    print(question1 - question2)
+
+subtraction()
+
+
+def division():
+    question1 = float(input("Please type in your first number you would like to use in division?"))
+    question2 =float(input("Please type in your second number you would like to use in division?"))
+    print(question1 / question2)
+
+division()
+
+def multiplication():
+    question1 = float(input("Please type in your first number you would like to use in multiplying?"))
+    question2 =float(input("Please type in your second number you would like to use multiplying?"))
+    print(question1 * question2)
+
+multiplication()
+
+def square():
+    question1 = float(input("Please type in your first number you would like to use in squares?"))
+    question2 =float(input("Please type in your second number you would like to use squares??"))
+    print(question1 ** question2)
+
+square()
+
+
+def practice1():
+    print((3 + 4)* 2)
+
+practice1()
+
+def wageprice():
+    hours_worked = 15.50
+    rate_per_hour = 10.50
+
+    wages = hours_worked*rate_per_hour
+
+    print(wages)
+
+wageprice()
