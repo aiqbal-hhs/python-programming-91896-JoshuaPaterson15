5ffb4d8f172deb9d42944e70eae530f1e7bcd885
JoshuaPaterson15 <65004100+JoshuaPaterson15@users.noreply.github.com>
2021-06-24 13:24:05 +1200

    Updates
    Extra work done.

--- boundary_testing.py
+++ boundary_testing.py
@@ -6,10 +6,13 @@ def menu_call(): #Definition created as the menu.
     if menu_option == 1: #If a users types in 1 as their answer, the menu would call the concession definition.
         consession()
         
-    elif menu_option == 2: #If a users types in 2 as their answer, the menu would call the tickets definition.
+    if menu_option == 2: #If a users types in 2 as their answer, the menu would call the tickets definition.
         print("You will be directed to the purchase page")
         time.sleep(4) #Using the import time function, I could pause the programe for 4 seconds.
         tickets() #This calls the tickets definition.
+
+    else:
+        print("Your input was wrong, please try again")
 
 def consession(): #Definition created for verifing someones eligbility for a consession ticket.
     age = float(input("Please insert your age which will tell us which consession you are entitled to? ")) #This numeric variable which has been named accordingly, is to identify how old the user is. 
@@ -60,11 +63,13 @@ Seniors (anyone aged aboved 60) = $10 (A discount)
 
     """.format(childe_tickets_total, child_tickets_total, adult_tickets_total, senior_tickets_total)) #This is the total prices based on the users request and is printed per catagory to outline the users request.
     print("Your total is ${} for all your tickets to Henderson High School's Production".format(grand_total)) #This is the overall total price when all the totals are calculated together.
+    return()
 
 
-#MAIN SCRIPT BELOW --------------------------------------------------------------------------------------------------------------
+#MAIN SCRIPT BELOW -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 name = input("Hello, what is your name? ") #This is an imput so the script can be personalized according to their name.
 print("Hello {}, welcome to Henderson High School's Production.".format(name)) #An input to greet them on to the python system.
 menu_call() #Calling the menu definition as its the users first time.
+purchase_request("If you would like to purchase these tickets please type 'purchase' or 'no'")
 
